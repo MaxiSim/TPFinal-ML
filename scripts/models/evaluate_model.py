4ac21f37db464d7931bce52937cac8f63794cd81
AgusManzano <amanzano@udesa.edu.ar>
2024-07-02 19:24:26 -0300

    bayesian tunning

--- scripts/models/evaluate_model.py
+++ scripts/models/evaluate_model.py
@@ -1,29 +1,47 @@
 import os
 import sys
+import argparse
+import logging
+import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-# Agregar la ruta al directorio raíz del proyecto para importar los módulos
+# Añadir el directorio raíz del proyecto al path para importar módulos
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
 sys.path.append(project_root)
 
-from src.models.linear_regression import LinearRegressionModel
+# Configuración de logging
+log_dir = os.path.join(project_root, 'logs')
+logging.basicConfig(filename=os.path.join(log_dir, 'evaluate.log'), level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
+
 from src.data.load_data import load_data
+from src.utils.utils import get_model
 
-# Cargar los datos
-data = load_data(os.path.join(project_root, 'data', 'data.csv'))
+def main(args):
+    model_name = args.model
+    model_path = args.model_path
+    filepath = os.path.join(project_root, 'data/data.csv')  # Ruta al archivo CSV
+    logging.info(f"Cargando datos desde {filepath}")
+    data = load_data(filepath)
+    
+    X = data.drop(columns=['Precio'])
+    y = data['Precio']
 
-# Separar características y objetivo
-X = data.drop(columns=['Precio'])  # Asume que la columna 'price' es el objetivo
-y = data['Precio']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    
+    logging.info(f"Cargando y evaluando modelo {model_name} desde {model_path}")
+    model = get_model(model_name)
+    model.load_model(model_path)
+    
+    mae, mse, rmse, r2 = model.evaluate(X_test, y_test)
+    logging.info(f"Resultados del modelo {model_name} - MAE: {mae}, MSE: {mse}, RMSE: {rmse}, R^2: {r2}")
+    
+    print(f"Modelo: {model_name}")
+    print(f"MAE: {mae}, MSE: {mse}, RMSE: {rmse}, R^2: {r2}")
 
-# Dividir en conjuntos de entrenamiento y prueba
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Crear el modelo y cargar el modelo guardado
-lr_model = LinearRegressionModel()
-lr_model.load_model(os.path.join(project_root, 'outputs', 'models', 'linear_regression_model.pkl'))
-
-# Evaluar el modelo
-mae, mse, rmse, r2 = lr_model.evaluate(X_test, y_test)
-print(f'MAE: {mae}, MSE: {mse}, RMSE: {rmse}, R^2: {r2}')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Evaluar un modelo de ML especificado.")
+    parser.add_argument('model', type=str, help="El nombre del modelo a usar (e.g., 'linear_regression', 'random_forest', 'xgboost')")
+    parser.add_argument('model_path', type=str, help="La ruta al modelo guardado")
+    args = parser.parse_args()
+    main(args)
--- scripts/models/train_model.py
+++ scripts/models/train_model.py
@@ -6,32 +6,20 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-
 # Añadir el directorio raíz del proyecto al path para importar módulos
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
 sys.path.append(project_root)
 
-log_dir = os.path.join(project_root, 'logs')
 # Configuración de logging
+log_dir = os.path.join(project_root, 'logs')
 logging.basicConfig(filename=os.path.join(log_dir, 'training.log'), level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
 
-
 from src.data.load_data import load_data
+from src.utils.utils import get_model
 
-def get_model(model_name):
-    if model_name == 'linear_regression':
-        from src.models.linear_regression import LinearRegressionModel
-        return LinearRegressionModel()
-    elif model_name == 'random_forest':
-        from src.models.random_forest import RandomForestModel
-        return RandomForestModel()
-    elif model_name == 'xgboost':
-        from src.models.xgboost import XGBoostModel
-        return XGBoostModel()
-    else:
-        raise ValueError(f"Modelo no soportado: {model_name}")
-
-def main(model_name):
+def main(args):
+    model_name = args.model
+    optimize = args.optimize
     filepath = os.path.join(project_root, 'data/data.csv')  # Ruta al archivo CSV
     logging.info(f"Cargando datos desde {filepath}")
     data = load_data(filepath)
@@ -42,9 +30,15 @@ def main(model_name):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
     logging.info(f"Entrenando modelo {model_name}")
-    model = get_model(model_name)
-    model.train(X_train, y_train)
+ 
+    if optimize:
+        model, params_opt = get_model(model_name, optimize=True, X_train=X_train, y_train=y_train)
+        logging.info(f"Mejores hiperparámetros encontrados: {params_opt}")
+    else:
+        model = get_model(model_name)
     
+    model.train(X_train, y_train)
+
     mae, mse, rmse, r2 = model.evaluate(X_test, y_test)
     logging.info(f"Resultados del modelo {model_name} - MAE: {mae}, MSE: {mse}, RMSE: {rmse}, R^2: {r2}")
     
@@ -53,6 +47,7 @@ def main(model_name):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Entrenar un modelo de ML especificado.")
-    parser.add_argument('model', type=str, help="El nombre del modelo a usar (e.g., 'linear_regression', 'random_forest', 'decision_tree', 'xgboost')")
+    parser.add_argument('model', type=str, help="El nombre del modelo a usar (e.g., 'linear_regression', 'random_forest', 'xgboost')")
+    parser.add_argument('--optimize', action='store_true', help="Optimizar los hiperparámetros del modelo", default=False)
     args = parser.parse_args()
-    main(args.model)
+    main(args)
