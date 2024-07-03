58000a4c58e4b2e1ddd3e81760709b071d1aa6bf
AgusManzano <amanzano@udesa.edu.ar>
2024-07-02 21:55:02 -0300

    evaluate ok

--- scripts/models/evaluate_model.py
+++ scripts/models/evaluate_model.py
@@ -15,11 +15,12 @@ log_dir = os.path.join(project_root, 'logs')
 logging.basicConfig(filename=os.path.join(log_dir, 'evaluate.log'), level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
 
 from src.data.load_data import load_data
-from src.utils.utils import get_model
+from src.utils.utils import *
 
 def main(args):
-    model_name = args.model
-    model_path = args.model_path
+    model_type = args.type
+    model_name = args.saved_model_name
+
     filepath = os.path.join(project_root, 'data/data.csv')  # Ruta al archivo CSV
     logging.info(f"Cargando datos desde {filepath}")
     data = load_data(filepath)
@@ -29,19 +30,28 @@ def main(args):
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
-    logging.info(f"Cargando y evaluando modelo {model_name} desde {model_path}")
-    model = get_model(model_name)
-    model.load_model(model_path)
-    
+    logging.info(f"Cargando y evaluando un {model_type} desde models/saved/{model_name}.joblib")
+    model = get_model(model_type) # Seleccionar el modelo a usar 
+
+    try:
+        model.load_model(os.path.join(project_root, 'src/models/saved', f'{model_name}.joblib'))
+    except FileNotFoundError:
+        logging.error(f"El archivo {model_name}.joblib no existe en {os.path.join(project_root, 'src/models/saved')}")
+        sys.exit(1)
+
     mae, mse, rmse, r2 = model.evaluate(X_test, y_test)
     logging.info(f"Resultados del modelo {model_name} - MAE: {mae}, MSE: {mse}, RMSE: {rmse}, R^2: {r2}")
     
     print(f"Modelo: {model_name}")
     print(f"MAE: {mae}, MSE: {mse}, RMSE: {rmse}, R^2: {r2}")
 
+    plot_predictions(y_test, model.predict(X_test))
+    # logging.info(f"Gráfico de predicciones guardado en {os.path.join(project_root, 'plots', 'predictions.png')}")
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Evaluar un modelo de ML especificado.")
-    parser.add_argument('model', type=str, help="El nombre del modelo a usar (e.g., 'linear_regression', 'random_forest', 'xgboost')")
-    parser.add_argument('model_path', type=str, help="La ruta al modelo guardado")
+    parser.add_argument('--type', type=str, help="El tipo de modelo a usar (e.g., 'linear_regression', 'random_forest', 'xgboost')")
+    parser.add_argument('saved_model_name', type=str, help="El nombre del modelo guardado (sin la extensión .joblib)")
     args = parser.parse_args()
     main(args)
--- scripts/models/train_model.py
+++ scripts/models/train_model.py
@@ -2,8 +2,6 @@ import os
 import sys
 import argparse
 import logging
-import numpy as np
-import pandas as pd
 from sklearn.model_selection import train_test_split
 
 # Añadir el directorio raíz del proyecto al path para importar módulos
@@ -18,8 +16,8 @@ from src.data.load_data import load_data
 from src.utils.utils import get_model
 
 def main(args):
-    model_name = args.model
-    optimize = args.optimize
+    model_type = args.model
+    
     filepath = os.path.join(project_root, 'data/data.csv')  # Ruta al archivo CSV
     logging.info(f"Cargando datos desde {filepath}")
     data = load_data(filepath)
@@ -29,25 +27,37 @@ def main(args):
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
-    logging.info(f"Entrenando modelo {model_name}")
+    logging.info(f"Entrenando modelo {model_type}")
  
-    if optimize:
-        model, params_opt = get_model(model_name, optimize=True, X_train=X_train, y_train=y_train)
+    if args.optimize:
+        model, params_opt = get_model(model_type, optimize=True, X_train=X_train, y_train=y_train)
         logging.info(f"Mejores hiperparámetros encontrados: {params_opt}")
     else:
-        model = get_model(model_name)
+        model = get_model(model_type)
     
     model.train(X_train, y_train)
 
     mae, mse, rmse, r2 = model.evaluate(X_test, y_test)
-    logging.info(f"Resultados del modelo {model_name} - MAE: {mae}, MSE: {mse}, RMSE: {rmse}, R^2: {r2}")
+    logging.info(f"Resultados del modelo {model_type} - MAE: {mae}, MSE: {mse}, RMSE: {rmse}, R^2: {r2}")
     
-    print(f"Modelo: {model_name}")
+    print(f"Modelo: {model_type}")
     print(f"MAE: {mae}, MSE: {mse}, RMSE: {rmse}, R^2: {r2}")
+
+    if args.save_model:
+        model.save_model(os.path.join(project_root, 'src/models/saved', f'{args.name}.joblib'))
+        logging.info(f"Modelo guardado en {os.path.join(project_root, 'src/models/saved', f'{model_type}.joblib')}")
+
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Entrenar un modelo de ML especificado.")
     parser.add_argument('model', type=str, help="El nombre del modelo a usar (e.g., 'linear_regression', 'random_forest', 'xgboost')")
-    parser.add_argument('--optimize', action='store_true', help="Optimizar los hiperparámetros del modelo", default=False)
+    parser.add_argument('--optimize', action='store_true', help="Optimizar los hiperparámetros del modelo")
+    parser.add_argument('--save_model', action='store_true', help="Guardar el modelo entrenado")
+    parser.add_argument('--name', type=str, help="El nombre del archivo del modelo guardado")
     args = parser.parse_args()
+
+    if args.save_model and not args.name:
+        parser.error("--save_model requires --name argument")
+
     main(args)
